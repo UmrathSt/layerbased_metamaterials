2204af9425e571d382fea40d26280d9dceb7ffd5
Stefan <Stefan.Umrath@dlr.de>
2017-08-07 13:13:32 +0200

    plus-absorber

---
+++ run_plus.m
@@ -0,0 +1,19 @@
+clear;
+clc;
+physical_constants;
+addpath("./libraries");
+
+
+
+w = 2;
+UCDim = 20;
+complemential = 0;
+fr4_thickness = 2;
+eps_FR4 = 4.6;
+tand = 0.015;
+fr4_thickness = 2;
+mesh_refinement = 1;
+for L = [6];
+  plus(UCDim, fr4_thickness, L, w, eps_FR4, tand, mesh_refinement, complemential);
+
+endfor;
