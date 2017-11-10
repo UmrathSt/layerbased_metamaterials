afb5d0c779afd52cd28a0031ac3cb3aa091c5582
umra_st <stefan.umrath@dlr.de>
2017-11-10 14:16:05 +0100

    update minkowski

--- python_scripts/minkowski_fractal.py
+++ python_scripts/minkowski_fractal.py
@@ -1,5 +1,10 @@
 import numpy as np
 from math import sqrt
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--no_iter", dest="no_iter", type=int)
+args = parser.parse_args()
+
 
 class point:
     def __init__(self, x, y):
@@ -33,56 +38,67 @@ class MinkowskiFractal:
         return self.points
 
     def split_points(self, pt1, pt2):
+        """ Split the line connecting pt1 and pt2 into 
+            self.number equal parts and return the outermost
+            points which should be inserted
+            Parameters:
+            -----------
+            pt1, pt2 : points
+            Returns:
+            ----------
+            list of two points, which should be inserted
+            Example:
+            ----------
+            pt1 = [0, 0], pt2 = [1, 0]
+            pt3 = [0.25. 0], pt3 = [0.5, 0]
+            -> Return is [pt3, pt4]
+        """
         dx = pt2.x - pt1.x
         dy = pt2.y - pt1.y
         number = self.number
         fact = number + 1
         pt3 = point(pt1.x + dx/fact, pt1.y + dy/fact)
-        pt4 = point(pt1.x + number*dx/fact, pt1.y + number*dy/fact)
+        pt4 = point(pt2.x - dx/fact, pt2.y - dy/fact)
         return [pt3, pt4]
 
     def shift_line(self, l):
         scale = self.scale
-        trans_x = scale*l.dir[1]*l.length
-        trans_y = -scale*l.dir[0]*l.length
+        trans_x = -(-scale*l.dir[1]*l.length)
+        trans_y = -(+scale*l.dir[0]*l.length)
         new_p1 = point(l.start.x - trans_x, l.start.y - trans_y)
         new_p2 = point(l.stop.x - trans_x, l.stop.y - trans_y)
         new_line = line(new_p1, new_p2)
         return new_line 
 
     def iterate(self):
-        new_points = []
+        """ Insert four points between neighbouring points
+        """
+        updated_points = []
         for i in range(len(self.points) - 1):
-            try:
-                insert_idx = i+1
-                to_insert = self.split_points(self.points[i], self.points[i+1])
-                new_points.append(self.points[i])
-                new_points.append(to_insert[0])
-                l = line(to_insert[0], to_insert[1])
-                shift_line = self.shift_line(l)
-                new_points.extend([shift_line.start, shift_line.stop])
-                new_points.append(to_insert[1])
-                new_points.append(self.points[i+1])
-            except:
-                pass
-        self.points = new_points            
+            new_pts = self.split_points(self.points[i], self.points[i+1])
+            updated_points.append(self.points[i])
+            updated_points.append(new_pts[0])
+            l = line(new_pts[0], new_pts[1])
+            shift_line = self.shift_line(l)
+            updated_points.extend([shift_line.start, shift_line.stop])
+            updated_points.append(new_pts[1])
+        updated_points.append(self.points[i+1])
+        self.points = updated_points            
 
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
     import numpy as np
-    points = [point(-1, 1), point(1, 1), 
-              point(1, -1), point(-1, -1),
-              point(-1, 1)]
-    mkf = MinkowskiFractal(points, -1, 2)
+    points = [point(-2, 2), point(2, 2), 
+              point(2, -2), point(-2, -2), point(-2, 2)]
+    mkf = MinkowskiFractal(points, -0.85, 2)
     i = 0
-    colors = ["k", "r", "b"]
-    while i < len(colors):
+    no_iterations = args.no_iter 
+    while i < no_iterations+1:
         kurve = np.array([[p.x, p.y] for p in mkf.get_points()])
-        #plt.plot(kurve[:, 0], kurve[:, 1], linestyle = "-", linewidth = 2, color = colors[i])
         mkf.iterate()
         i += 1
-    plt.plot(kurve[:, 0], kurve[:, 1], linestyle = "-", linewidth = 4, color = colors[i-1])
+    plt.plot(kurve[:, 0], kurve[:, 1], linestyle = "-", linewidth = 4, color="k")
     plt.axis("equal")
-    plt.xlim([-1.5, 1.5])
-    plt.ylim([-1.5,1.5])
+    plt.xlim([-2.5, 2.5])
+    plt.ylim([-2.5, 2.5])
     plt.show()
