513596b2ffbff2e670876d4569f46e69cf134382
umra_st <stefan.umrath@dlr.de>
2018-02-28 15:48:35 +0100

    update

--- python_scripts/slab_scattering.py
+++ python_scripts/slab_scattering.py
@@ -11,6 +11,7 @@ class SlabStructure:
         self.M = len(Z) - 1 # number of interfaces 
         self.l = l
         self.k = k
+        self.tau = Z[1:,:]*2/(Z[0:-1,:]+Z[1:,:])
         self.rho = (Z[1:,:]-Z[0:-1,:]) / (Z[1:,:]+Z[0:-1,:])
         assert Z.shape[0]-2 == l.shape[0] == k.shape[0]
     
@@ -27,26 +28,47 @@ class SlabStructure:
             GammaM = (rho[i,:] + GammaM*phasefactor) / (1 + rho[i,:]*GammaM*phasefactor )
         return GammaM
         
+    def build_tau(self):
+        """ Get the left to right transmission 
+            of the stacked structure
+        """
+        Z = self.Z[-2::-1,:]
+        rho = -self.rho[-2::-1,:]
+        phase = (self.k*self.l)[-2::-1,:]
+        GammaM = rho[-1,:]
+        print("In build_tau: self.rho = ", self.rho)
+        print("In build_tau: GammaM = ", GammaM)
+        for i in range(rho.shape[0]-2, -1, -1):
+            print("Building tau: i=%i" %i)
+            phasefactor = np.exp(2j*phase[i,:])
+            GammaM = (rho[i,:] + GammaM*phasefactor) / (1 + rho[i,:]*GammaM*phasefactor )
+        return 1 + 1/GammaM
 
+        
+        
 
 
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
-    tand1 = 0.02
-    eps1 = 4.4
+    tand1 = 9.302e-3
+    eps1 = 2.795 
     epsFR4 = eps1*(1 + tand1*1j)
     eps2 = 9
-    tand2 = 0.111
+    tand2 = 0
+    Z0 = 376.3
     epsRubber = eps2*(1 + tand2*1j)
     eps = np.array([epsFR4])[:,np.newaxis]
-    f = (np.linspace(0,20,1000)*1e9)[np.newaxis,:]
-    Zlist = np.array([376.73,1/np.sqrt(epsFR4), 0])[:,np.newaxis]
-    l = np.array([3.58e-3])[:,np.newaxis]
+    f = (np.linspace(70,100,1000)*1e9)[np.newaxis,:]
+    Zlist = np.array([Z0,Z0/np.sqrt(epsFR4), Z0])[:,np.newaxis]
+    l = np.array([50e-3])[:,np.newaxis]
     k = np.sqrt(eps)*2*np.pi*f/3e8
     slabstack = SlabStructure(Zlist, l, k)
     R = slabstack.build_gamma()
     print(R.shape)
-    plt.plot(f[0,:]/1e9, 20*np.log10(np.abs(R)))
+    T = slabstack.build_tau()
+    print("T = \n", T)
+    plt.plot(f[0,:]/1e9, 20*np.log10(np.abs(R)),"r-", label="R")
+#    plt.plot(f[0,:]/1e9, 20*np.log10(np.abs(T)),"b-", label="T")
     plt.xlabel("f [GHz]")
     plt.ylabel("$20\log|S_{11}|)$")
     plt.show()
--- run_splitring.m
+++ run_splitring.m
@@ -3,17 +3,17 @@ clc;
 physical_constants;
 addpath('./libraries');
 
+scale = 0.5;
 
-
-UCDim = 20;
+UCDim = 20*scale;
 complemential = 0;
 fr4_thickness = 2;
 eps_FR4 = 4.4;
 tand = 0.015;
 fr4_thickness = 2;
-mesh_refinement = 1;
-R = 9.8
-w = 1.5
+mesh_refinement = 1.5;
+R = 9.8*scale
+w = 1.5*scale
 phi0 = 3*pi/4;
 DeltaPhi = pi/16;
 for DeltaPhi = [pi/20, pi/25];
