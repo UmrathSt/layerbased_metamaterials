90717e780c558685e17d19c3a68ba3ec4018bc89
umra_st <stefan.umrath@dlr.de>
2018-04-19 14:07:30 +0200

    update

---
+++ python_scripts/TL_Model_Perforated_Plate/AbsorptionCalc.py
@@ -0,0 +1,30 @@
+# Compare the absorption of a metamaterial absorber when calculated directly
+# from an absorber with 0.4 mm FR4 thickness
+# and by an impedance calculation where the respective impedance of the metalayer
+# is parallel connected to the grounded dielectric slab
+import numpy as np
+from matplotlib import pyplot as plt
+
+absorber = np.loadtxt("absorber_2.25.txt", delimiter=",")
+fss = np.loadtxt("fss_2.25.txt", delimiter=",")
+
+# the analytical formula for a 0.4 mm thick FR4 slab with eps_r = 4.4 and
+# a frequency dependend imaginary part such that a constant conductivity
+# is emulated in the respective FDTD
+
+f = fss[:,0]
+epsFR4 = 4.4 - 1j*0.048957/(2*np.pi*f*8.85e-12)
+lz = 0.4e-3 # fr4 thickness
+Zslab = 1j*377/np.sqrt(epsFR4)*np.tan(2*np.pi*f/3e8*np.sqrt(epsFR4)*lz)
+D = 40.25e-3
+phase = np.exp(4j*np.pi*f*D/3e8)
+Rfss = (fss[:,1] + 1j * fss[:,2]) * phase
+Zfss = 377*(1 + Rfss) / (1 - Rfss)
+Zmeta = Zfss*Zslab/(Zfss+Zslab)
+
+plt.plot(f/1e9, np.abs(absorber[:,1]+1j*absorber[:,2]), "r-", label="absorber")
+plt.plot(f/1e9, np.abs((Zmeta - 377) / (Zmeta + 377)), "b-", label="fss")
+plt.xlabel("f [GHz]")
+plt.ylabel("|S11|")
+plt.legend()
+plt.show()
--- python_scripts/TL_Model_Perforated_Plate/Zcomparison.py
+++ python_scripts/TL_Model_Perforated_Plate/Zcomparison.py
@@ -3,18 +3,29 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-data = np.loadtxt("fss_2.25.txt", delimiter=",")
+data = np.loadtxt("slab_0.4_2.25.txt", delimiter=",") # 0.4 mm FR4 file with increasing mesing
+#data = np.loadtxt("slab_4_2.25.txt", delimiter=",") # 4 mm FR4 file 
+
 
 f = data[:,0] # in GHz
-S11 = data[:,1] + 1j*data[:,2]
-Z = data[:,5] + 1j*data[:,6]
+D = 76.8541*1e-3 # 0.4 mm fr4 thickness
+#D = (77.7256-4.5)*1e-3 # 4 mm fr4 thickness
+S11 = (data[:,1] + 1j*data[:,2])*np.exp(4j*np.pi*f/3e8*D)
+Z = (data[:,5] + 1j*data[:,6])*np.exp(4j*np.pi*f/3e8*D)
+epsFR4 = 4.4 -1j*0.048957/(2*np.pi*f*8.8542e-12)
+lz = 4e-4 # 0.4 mm FR4
+Zslab = 1j*377/np.sqrt(epsFR4)*np.tan(2*np.pi*f/3e8*np.sqrt(epsFR4)*lz)
 
-plt.plot(f/1e9, np.real((1+S11)/(1-S11)), "b-", label="Re(Z) from S")
-plt.plot(f/1e9, np.imag((1+S11)/(1-S11)), "r-", label="Im(Z) from S")
-plt.plot(f/1e9, np.real(Z)/377, "bo", markersize=2,label="Re(Z)")
-plt.plot(f/1e9, np.imag(Z)/377, "ro", markersize=2,label="Im(Z)")
+
+plt.plot(f/1e9, np.real((1+S11)/(1-S11))*377, "b-", label="Re(Z) from S")
+plt.plot(f/1e9, np.imag((1+S11)/(1-S11))*377, "r-", label="Im(Z) from S")
+#plt.plot(f/1e9, np.real(Z), "bo", markersize=2,label="Re(Z)")
+#plt.plot(f/1e9, np.imag(Z), "ro", markersize=2,label="Im(Z)")
+plt.plot(f/1e9, np.real(Zslab), "c-", label="Re(Z) analytic")
+plt.plot(f/1e9, np.imag(Zslab), "m-", label="Im(Z) analytic")
+
 plt.legend()
 plt.xlim([2,8])
-plt.ylim([-2,5])
+plt.ylim([-2,160])
 plt.show()
 
