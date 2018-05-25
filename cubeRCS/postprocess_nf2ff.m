97c636725cc3c7b14e7dae98974ebc873a79d715
Stefan <Stefan.Umrath@dlr.de>
2018-05-25 08:48:15 +0200

    correct nf2ffpath and simpath

--- cubeRCS/postprocess_nf2ff.m
+++ cubeRCS/postprocess_nf2ff.m
@@ -1,4 +1,4 @@
-function postprocess_nf2ff(Sim_Path, f, theta, theta_off, phi, phi_off, paramstr, Mirror= {1, 'PMC', 0})
+function postprocess_nf2ff(nf2ff_struct_Path, Sim_Path, f, theta, theta_off, phi, phi_off, paramstr, Mirror= {1, 'PMC', 0})
    % read nf2ff_structure in directory 'Path', and calculate nf2ff
    % data at frequency f at angles theta, phi in radian
    physical_constants;
@@ -6,7 +6,7 @@ function postprocess_nf2ff(Sim_Path, f, theta, theta_off, phi, phi_off, paramstr
    for F = f;
     EF = ReadUI( 'et', Sim_Path, F ); % time domain/freq domain voltage
     Pin = 0.5*norm(E_dir)^2/Z0 .* abs(EF.FD{1}.val).^2;
-    load([Sim_Path 'nf2ff_struct.dat']);
+    load([nf2ff_struct_Path 'nf2ff_struct.dat']);
     nf2ff = CalcNF2FF(nf2ff, Sim_Path, F, theta+theta_off, phi+phi_off, 'Mode',1, 'Mirror', Mirror);
     rcs = 4*pi*nf2ff.P_rad{1}./Pin(1);
     rcs_fname =  strcat('rcs_f_', num2str(f/1e9), '_GHz.dat');
