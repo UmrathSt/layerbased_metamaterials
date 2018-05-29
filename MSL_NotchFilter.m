a76c1b368694d7ab8d155ae055eb571092ff978e
umra_st <stefan.umrath@dlr.de>
2018-05-29 14:24:40 +0200

    geometry and meshing update

--- MSL_NotchFilter.m
+++ MSL_NotchFilter.m
@@ -17,12 +17,15 @@ clc
 %% setup the simulation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 physical_constants;
 unit = 1e-3; % specify everything in um
-MSL_length = 50.000;
-MSL_width = 1.6;
+MSL_length = 25.000;
+MSL_width = 1.5;
 substrate_thickness = 3.2;
 substrate_epr = 4.6;
-stub_length = 12;
-f_max = 7e9;
+stub_length = 1;
+f_max = 1e9;
+addpath('./libraries');
+
+
 
 %% setup FDTD parameters & excitation function %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 FDTD = InitFDTD();
@@ -32,46 +35,52 @@ FDTD = SetBoundaryCond( FDTD, BC );
 
 %% setup CSXCAD geometry & mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 CSX = InitCSX();
-resolution = c0/(f_max*sqrt(substrate_epr))/unit /50; % resolution of lambda/50
-mesh.x = SmoothMeshLines( [0 MSL_width/2+[2*resolution/3 -resolution/3]/4], resolution/4, 1.5 ,0 );
-mesh.x = SmoothMeshLines( [-MSL_length -mesh.x mesh.x MSL_length], resolution, 1.5 ,0 );
-mesh.y = SmoothMeshLines( [0 MSL_width/2+[-resolution/3 +resolution/3*2]/4], resolution/4 , 1.5 ,0);
+resolution = c0/(f_max*sqrt(substrate_epr))/unit /40; % resolution of lambda/50
+mesh.x = SmoothMeshLines( [0 MSL_width/2+[2*resolution/3 -resolution/3]/20], resolution/4, 1.5 ,0 );
+mesh.x = SmoothMeshLines( [-MSL_length -mesh.x -0.15, 0.15 mesh.x MSL_length], resolution, 1.5 ,0 );
+mesh.y = SmoothMeshLines( [0 MSL_width/2+[-resolution/3 +resolution/3*2]/20], resolution/4 , 1.5 ,0);
 mesh.y = SmoothMeshLines( [-15*MSL_width -mesh.y mesh.y stub_length+[-resolution/3 +resolution/3*2]/4 15*MSL_width+stub_length], resolution, 1.3 ,0);
-mesh.z = SmoothMeshLines( [linspace(0,substrate_thickness,5) 10*substrate_thickness], resolution );
+mesh.z = SmoothMeshLines( [linspace(0,substrate_thickness,5), substrate_thickness+linspace(0,0.35,5), 5*substrate_thickness], resolution/10 );
 CSX = DefineRectGrid( CSX, unit, mesh );
 
 %% substrate
-CSX = AddMaterial( CSX, 'RO4350B' );
-CSX = SetMaterialProperty( CSX, 'RO4350B', 'Epsilon', substrate_epr );
+CSX = AddMaterial( CSX, 'FR4' );
+CSX = SetMaterialProperty( CSX, 'FR4', 'Epsilon', substrate_epr );
 start = [mesh.x(1),   mesh.y(1),   0];
 stop  = [mesh.x(end), mesh.y(end), substrate_thickness];
-CSX = AddBox( CSX, 'RO4350B', 0, start, stop );
+CSX = AddBox( CSX, 'FR4', 0, start, stop );
 
 %% MSL port
 CSX = AddMetal( CSX, 'PEC' );
 portstart = [ mesh.x(1), -MSL_width/2, substrate_thickness];
-portstop  = [ 0,  MSL_width/2, 0];
+portstop  = [ -0.25,  MSL_width/2, 0];
 [CSX,port{1}] = AddMSLPort( CSX, 999, 1, 'PEC', portstart, portstop, 0, [0 0 -1], 'ExcitePort', true, 'FeedShift', 10*resolution, 'MeasPlaneShift',  MSL_length/3);
 
 portstart = [mesh.x(end), -MSL_width/2, substrate_thickness];
-portstop  = [0          ,  MSL_width/2, 0];
+portstop  = [0.25          ,  MSL_width/2, 0];
 [CSX,port{2}] = AddMSLPort( CSX, 999, 2, 'PEC', portstart, portstop, 0, [0 0 -1], 'MeasPlaneShift',  MSL_length/3 );
 
 %% Filter-stub
-start = [-MSL_width/2,  MSL_width/2, substrate_thickness];
-stop  = [ MSL_width/2,  MSL_width/2+stub_length, substrate_thickness];
-CSX = AddBox( CSX, 'PEC', 999, start, stop );
+%start = [-MSL_width/2,  MSL_width/2, substrate_thickness];
+%stop  = [ MSL_width/2,  MSL_width/2+stub_length, substrate_thickness];
+%CSX = AddBox( CSX, 'PEC', 999, start, stop );
+smdres.unit = 1e-3;
+smdres.Resistance = 100;
+smdres.prio = 10;
+smdres.xycenter = [0,0];
+[CSX, params] = CreateSMDResistor(CSX, smdres, [0,0,substrate_thickness+0.35/2], pi/2);
  
 %% write/show/run the openEMS compatible xml-file
-Sim_Path = 'tmp';
+Sim_Path = '/mnt/hgfs/E/openEMS/layerbased_metamaterials/Simulation/MSL_Notchfilter';
 Sim_CSX = 'msl.xml';
 
 [status, message, messageid] = rmdir( Sim_Path, 's' ); % clear previous directory
+confirm_recursive_rmdir(1);
 [status, message, messageid] = mkdir( Sim_Path ); % create empty simulation folder
 
 WriteOpenEMS( [Sim_Path '/' Sim_CSX], FDTD, CSX );
 CSXGeomPlot( [Sim_Path '/' Sim_CSX] );
-RunOpenEMS( Sim_Path, Sim_CSX );
+RunOpenEMS( Sim_Path, Sim_CSX, '--engine=multithreaded --numThreads=1');
 
 %% post-processing
 close all
