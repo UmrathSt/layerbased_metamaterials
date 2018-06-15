78d3cd916d0a3a07352208b7b82c8df3e276b30d
Stefan <Stefan.Umrath@dlr.de>
2018-06-15 07:50:09 +0200

    update for commit

--- sweep_squares.m
+++ sweep_squares.m
@@ -9,10 +9,11 @@ UCDim = 20;
 L1 = 16;
 L2 = 0;
 complemential = 0;
-fr4_thickness = 0.5
+fr4_thickness = 3.2
 eps_FR4 = 4.6;
+film_lz = 0.2;
 
 Rsq = 15;
-for Rsq = [250, 300, 350, 400, 450];
-    squares(UCDim, fr4_thickness, L1, L2, eps_FR4, Rsq, complemential);
+for Rsq = [100,250, 300, 350, 400, 450];
+    squares(UCDim, fr4_thickness, film_lz, L1, L2, eps_FR4, Rsq, complemential);
 end;
