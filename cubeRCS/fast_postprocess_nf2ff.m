28cd2e8f74e9fcd23f80384fbcc7c068b2a69878
Stefan <Stefan.Umrath@dlr.de>
2018-06-29 15:04:36 +0200

    update

--- cubeRCS/fast_postprocess_nf2ff.m
+++ cubeRCS/fast_postprocess_nf2ff.m
@@ -1,19 +1,29 @@
-function nf2ff = fast_postprocess_nf2ff(nf2ff_struct_Path, Sim_Path, f, theta, theta_off, phi, phi_off, paramstr,  namestr, Mirror= {1, 'PMC', 0})
-   % read nf2ff_structure in directory 'Path', and calculate nf2ff
-   % data at frequency f at angles theta, phi in radian
-   physical_constants;
-   E_dir = [cos(theta_off), 0, -sin(theta_off)];
-   EF = ReadUI('et', Sim_Path, f);
-   Pin = 0.5*norm(E_dir)^2/Z0 .* abs(EF.FD{1}.val).^2;
+function nf2ff = fast_postprocess_nf2ff(nf2ff_struct_Path, Sim_Path, f, theta, theta_off, phi, phi_off, paramstr,  namestr, Mirror= {0, 'PEC', 0})
+    % read nf2ff_structure in directory 'Path', and calculate nf2ff
+    % data at frequency f at angles theta, phi in radian
+    physical_constants;
+    E_dir = [cos(theta_off), 0, -sin(theta_off)];
+    EF = ReadUI('et', Sim_Path, f);
+    Pin = 0.5*norm(E_dir)^2/Z0 .* abs(EF.FD{1}.val).^2;
     load([nf2ff_struct_Path 'nf2ff_struct.dat']);
-    nf2ff = CalcNF2FF(nf2ff, Sim_Path, f, theta+theta_off, phi+phi_off, 'Mode',1, 'Mirror', Mirror);
-    fprintf('Prad{1} \n');
-    nf2ff.P_rad{1}
-    fprintf('Pin(1) \n');
-    Pin(1)
-   for Fn = 1:numel(f);
+    mir = 'yes';
+    try;
+        if strcmp(Mirror,'None');
+            mir = 'None';
+        end;
+    catch lasterror;
+    end;
     
-    rcs = 4*pi*nf2ff.P_rad{Fn}./Pin(Fn);
+
+    if ~strcmp(mir, 'None');
+        display('Using symmetries for the nf2ff');
+        nf2ff = CalcNF2FF(nf2ff, Sim_Path, f, theta+theta_off, phi+phi_off, 'Mode',1, 'Mirror', Mirror);
+    else;
+        display('Using no symmetries');
+        nf2ff = CalcNF2FF(nf2ff, Sim_Path, f, theta+theta_off, phi+phi_off, 'Mode',1);
+    end;
+       for Fn = 1:numel(f);
+    rcs = 4*pi./Pin(Fn).*nf2ff.P_rad{Fn}(1);
     rcs_fname =  strcat('rcs_', namestr, '_f_', num2str(f(Fn)/1e9,'%.2f'), '_GHz.dat');
     fid = fopen(rcs_fname, 'wt');
     fprintf(fid, ['# ' paramstr '\n']);
--- cubeRCS/sweepPECcube_inc_angle.m
+++ cubeRCS/sweepPECcube_inc_angle.m
@@ -1,18 +1,18 @@
-dump_TD = 1; % don't dump time-domain field-data 
+dump_TD = 0; % don't dump time-domain field-data 
 show_geom = 0; % don't show the geometry on every run
 angles =[0];%linspace(0, pi/4, 91);%linspace(0, pi/8, 46); % steps of 1 degree
-freq = 20e9; % frequencies for which to obtain the RCS
-Path = '/mnt/hgfs/E/openEMS/PECsphere_RCS_1-20GHz/';
+freq = 10e9; % frequencies for which to obtain the RCS
+Path = '/mnt/hgfs/E/openEMS/PECcube_RCS_1-10GHz/';
 L = 50; % mm
 if strcmp(uname.nodename, 'Xeon');
-    Path = '/media/stefan/Daten/openEMS/PECsphere_RCS_1-20GHz/' 
+    Path = '/media/stefan/Daten/openEMS/PECcube_RCS_1-10GHz/' 
 end;
 
 physical_constants;
 
 for i_alpha =1:1;
    clear -x c0 dump_TD show_geom angles i_alpha rcs_at_angles freq Path epsRe epsIm L;
-   rcs = bistat_PECsphere_rcs(angles(i_alpha), freq, L, dump_TD, show_geom, Path);
+   rcs = bistat_PECcube_rcs(angles(i_alpha), freq, L, dump_TD, show_geom, Path);
    rcs_at_angles{i_alpha} = rcs;
 end;
 
