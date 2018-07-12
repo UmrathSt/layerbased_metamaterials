90967c2806c80019a9c2603ce03eaba1a007f963
umra_st <stefan.umrath@dlr.de>
2018-07-12 08:00:08 +0200

    woodpile

--- sweep_woodpile.m
+++ sweep_woodpile.m
@@ -4,12 +4,12 @@ physical_constants;
 addpath('./libraries');
 
 UCDim = 12;
-w = 5;
+w = 8;
 z = 0.1;
 l_subs = 2;
-params = {w/2, w/2, l_subs, 'ConductingFilament', 1, 100, 'FR4', 4.6, 0.1;
-                   w,UCDim,z,            'Cu', 1, 56e6, 'air', 1, 0};
+params = {UCDim-2, UCDim-2, l_subs, 'ConductingFilament', 1.5, 5, 'air', 1, 0;
+                   UCDim-2,UCDim-2,z,            'Cu', 1, 56e6, 'air', 1, 0};
 complemential = 0;
 
-woodpile('1', UCDim, params, complemential);
+woodpile('3', UCDim, params, complemential);
 
