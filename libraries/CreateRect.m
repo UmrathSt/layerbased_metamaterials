41fca51af0d5b554e82804dd3b2b20af922a478a
umra_st <stefan.umrath@dlr.de>
2018-07-12 08:34:49 +0200

    took care of missing mesh_lines in the return

--- libraries/CreateRect.m
+++ libraries/CreateRect.m
@@ -1,8 +1,14 @@
-function [CSX, params] = CreateRect(CSX, object, translate, rotate);
+function [CSX, params, mesh_lines] = CreateRect(CSX, object, translate, rotate);
+    % create a homogenious rectangle which takes care of the mesh lines
+    % itself.
   object = object;
   diag = [object.lx/2, object.ly/2, object.lz/2];
   box_start = -diag;
   box_stop = +diag;
+  % this object needs no special meshlines
+  mesh_lines.x = [];
+  mesh_lines.y = [];
+  mesh_lines.z = [];
   CSX = AddBox(CSX, object.material.name, object.prio, box_start, box_stop,
          'Transform', {'Rotate_Z', rotate, 'Translate', translate});
   ocenter = [object.xycenter(1:2), 0] + translate;
